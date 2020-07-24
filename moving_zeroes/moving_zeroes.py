11df182dd4cded6751605494b76128b48e22beac
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-07-23 17:57:08 -0700

    mvp

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,46 +4,46 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-    # zeroes = []
-    # nonZeroes = []
+    zeroes = []
+    nonZeroes = []
 
-    # for i in arr:
-    #     if i == 0:
-    #         zeroes.append(i)
-    #     else:
-    #         nonZeroes.append(i)
+    for i in arr:
+        if i == 0:
+            zeroes.append(i)
+        else:
+            nonZeroes.append(i)
     # return nonZeroes + zeroes
     
     # Improved method
     # initialize a left and right pointer
     # left is 0
     # right is last index in arr
-    i = 0
-    left = arr[i]
-    right = arr[len(arr)-1]
-    new_arr = []
-    # while left <= right:
-    while left <= right:
-        # if left points at a zero and right points at non-zero
-        if left == 0 and right != 0:
-            # swap left and right items in original arr
-            new_arr.insert(right, left)
-            # increment left
-            i += 1
-            # decrement right
-            right -= 1
-        # else
-        else:
-            # if left is non-zero:
-            if left != 0:
-                # increment left
-                i += 1
-            # if right is zero:
-            if right == 0:
-                # decrement right
-                right -= 1
+    # left = 0
+    # right = len(arr)
+    # new_arr = []
+    # # while left <= right:
+    # while left < right:
+    #     # if left points at a zero and right points at non-zero
+    #     if arr[left] == 0 and arr[right-1] != 0:
+    #         # swap left and right items in original arr
+    #         new_arr.append(arr[right-1])
+    #         # # increment left
+    #         left += 1
+    #         # # decrement right
+    #         right -= 1
+    #     # else
+    #     else:
+    #         new_arr.append(arr[left])
+    #         # if left is non-zero:
+    #         if arr[left] != 0:
+    #             # increment left
+    #             left += 1
+    #         # if right is zero:
+    #         if arr[right-1] == 0:
+    #         #     # decrement right
+    #             right -= 1
                 
-    return new_arr
+    # return new_arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
